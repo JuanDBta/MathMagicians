fb786c1c04bdbcb9934184457a13631b9d83b072
JuanDBta <juandavid_diaz@hotmail.com>
2023-06-29 17:13:40 -0500

    Add Home component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Link, Outlet,
 } from 'react-router-dom';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quotes';
 import './App.css';
@@ -10,6 +11,7 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
+        <Route path="/home" element={<Home />} />
         <Route path="/calculator" element={<Calculator />} />
         <Route path="/quote" element={<Quote />} />
       </Route>,
@@ -28,6 +30,7 @@ const Root = () => (
     <nav className="navbar">
       <h1 className="navbar-title">Math Magicians</h1>
       <ul className="navbar-list">
+        <li><Link to="/home" className="navbar-item">Home</Link></li>
         <li><Link to="/calculator" className="navbar-item">Calculator</Link></li>
         <li><Link to="/quote" className="navbar-item">Quote</Link></li>
       </ul>
