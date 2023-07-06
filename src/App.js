821c2b1d75b1a73a08fe986be087ce5605328e5e
JuanDBta <juandavid_diaz@hotmail.com>
2023-07-06 17:46:55 -0500

    Modify Route in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Link, Outlet,
+  BrowserRouter as Router, Routes, Route, Link,
 } from 'react-router-dom';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
@@ -8,37 +8,29 @@ import Quote from './components/Quotes';
 import './App.css';
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Root />}>
-        <Route path="/home" element={<Home />} />
-        <Route path="/calculator" element={<Calculator />} />
-        <Route path="/quote" element={<Quote />} />
-      </Route>,
-    ),
-  );
-
   return (
     <div className="body">
-      <RouterProvider router={router} />
+      <Router>
+        <nav className="navbar">
+          <h1 className="navbar-title">Math Magicians</h1>
+          <ul className="navbar-list">
+            <li><Link to="/home" className="navbar-item">Home</Link></li>
+            <li><Link to="/calculator" className="navbar-item">Calculator</Link></li>
+            <li><Link to="/quote" className="navbar-item">Quote</Link></li>
+          </ul>
+        </nav>
+
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/quote" element={<Quote />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
-
-const Root = () => (
-  <>
-    <nav className="navbar">
-      <h1 className="navbar-title">Math Magicians</h1>
-      <ul className="navbar-list">
-        <li><Link to="/home" className="navbar-item">Home</Link></li>
-        <li><Link to="/calculator" className="navbar-item">Calculator</Link></li>
-        <li><Link to="/quote" className="navbar-item">Quote</Link></li>
-      </ul>
-    </nav>
-    <div>
-      <Outlet />
-    </div>
-  </>
-);
 
 export default App;
